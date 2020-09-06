5fe0cc4275dd9fbf4d504c6dd8810d5bb15860a8
Miguel Maciel <miguelamorimmaciel@gmail.com>
2020-09-06 15:45:24 +0100

    Lets Start first commit

--- app/src/main/java/android/example/gitapplicationdemo/MainActivity.java
+++ app/src/main/java/android/example/gitapplicationdemo/MainActivity.java
@@ -3,6 +3,11 @@ package android.example.gitapplicationdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText etName;
+        final TextView tvName;
+        Button btnHello;
+
+        etName = findViewById(R.id.etName);
+        tvName = findViewById(R.id.tvHelloName);
+        btnHello = findViewById(R.id.button);
+
+        btnHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvName.setText("Hello " + etName.getText().toString());
+            }
+        });
     }
 }
